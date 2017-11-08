9dbfb088ac99fb66e84dd846c2fcda260f740090
Shubham Ramdeo <ramdeo.shubham@gmail.com>
2017-11-08 10:09:33 +0530

    Update beauter.js

--- beauter.js
+++ beauter.js
@@ -1,127 +1,58 @@
 function showsnackbar(y) {
     var x = document.getElementById(y);
-    x.className = x.className.replace("snackbar", "snackbar show"); 
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    x.className = x.className.replace("snackbar", "snackbar show");
+    setTimeout(function() {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
 
 
 function topnav(y) {
-            var x = document.getElementById(y);
-            if (!x.classList.contains('responsive'))    {
-                x.className += " responsive";
-            } else {
-				x.className = x.className.replace("responsive", "");
-            }
+    var x = document.getElementById(y);
+    if (!x.classList.contains('responsive')) {
+        x.className += " responsive";
+    } else {
+        x.className = x.className.replace("responsive", "");
+    }
+}
+
+function openmodal(idmodal) {
+    var modal = document.getElementById(idmodal);
+    modal.style.display = "block";
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
+    }
+}
 
+function openimg(imgi, modali) {
+    var img = document.getElementById(imgi);
+    var modal = document.getElementById(modali);
+    var modalImg = modal.getElementsByClassName("modal-content")[0];
+    var captionText = modal.getElementsByClassName("caption")[0];
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
 
 
 var close = document.getElementsByClassName("-close");
 var i;
 for (i = 0; i < close.length; i++) {
-    close[i].onclick = function(){
+    close[i].onclick = function() {
         var div = this.parentElement;
-        div.style.opacity = "0";       
-        setTimeout(function(){ div.style.display = "none"; }, 600);
-    }
-}
-
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal1');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn1");
-
-// Get the <span> element that closes the modal
-var span = document.getElementById("modalbox-close1");
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-////iimghbox
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementById("imgclose");
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////TABS
-
-
-
-var close = document.getElementsByClassName("-close");
-var i;
-
-for (i = 0; i < close.length; i++) {
-   
-    close[i].onclick = function(){
-
-               var div = this.parentElement;
-
-        
         div.style.opacity = "0";
-
-       
-        setTimeout(function(){ div.style.display = "none"; div.style.opacity = "1";}, 600);
+        setTimeout(function() {
+            div.style.display = "none";
+            div.style.opacity = "1";
+        }, 600);
     }
 }
 
-function openCity(evt, cityName) {
+
+function opentab(cityName, evt) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -135,5 +66,4 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
