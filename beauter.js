b8a96a95e6e5f2fce2de49b54ae365037b6496cc
Shubham Ramdeo <ramdeo.shubham@gmail.com>
2017-11-13 13:56:22 +0530

    Update beauter.js

--- beauter.js
+++ beauter.js
@@ -67,3 +67,18 @@ function opentab(tabname, evt) {
 }
 
 document.getElementById("defaultOpen").click();
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
